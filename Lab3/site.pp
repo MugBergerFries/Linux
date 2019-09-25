848d49585e7b728630255cb8b4ab7a9610ac7da0
MugBergerFries <sjamesberger@gmail.com>
2019-09-24 23:02:49 -0600

    added lab2.txt and site.pp for lab 3

---
+++ Lab3/site.pp
@@ -0,0 +1,200 @@
+group { "managers":
+    gid => "1999"
+}
+
+group { "accounting":
+    gid => "2999"
+}
+
+group { "sales":
+    gid => "3999"
+}
+
+user { "mscott":
+    uid => "1001",
+    ensure => present,
+    comment => "Michael Scott",
+    gid => "1001"
+    groups => ["managers"],
+    shell => "/bin/bash",
+    home => "/home/mscott",
+    managehome => true,
+}
+
+user { "dschrute":
+    uid => "1002",
+    ensure => present,
+    comment => "Dwight Schrute",
+    gid => "1002"
+    groups => ["managers"],
+    shell => "/bin/bash",
+    home => "/home/dschrute",
+    managehome => true,
+}
+
+user { "jhalpert":
+    uid => "1003",
+    ensure => present,
+    comment => "Jim Halpert",
+    gid => "1003"
+    groups => ["managers"],
+    shell => "/bin/bash",
+    home => "/home/jhalpert",
+    managehome => true,
+}
+
+user { "amartin":
+    uid => "2001",
+    ensure => present,
+    comment => "Angela Martin",
+    gid => "2001"
+    groups => ["accounting"],
+    shell => "/bin/bash",
+    home => "/home/amartin",
+    managehome => true,
+}
+
+user { "kmalone":
+    uid => "2002",
+    ensure => present,
+    comment => "Kevin Malone",
+    gid => "2002"
+    groups => ["accounting"],
+    shell => "/bin/bash",
+    home => "/home/kmalone",
+    managehome => true,
+}
+
+user { "omartinez":
+    uid => "2003",
+    ensure => present,
+    comment => "Oscar Martinez",
+    gid => "2003"
+    groups => ["accounting"],
+    shell => "/bin/bash",
+    home => "/home/omartinez",
+    managehome => true,
+}
+
+user { "tflenderson":
+    uid => "2004",
+    ensure => present,
+    comment => "Toby Flenderson",
+    gid => "2004"
+    groups => ["accounting"],
+    shell => "/bin/bash",
+    home => "/home/tflenderson",
+    managehome => true,
+}
+
+user { "cbratton":
+    uid => "2005",
+    ensure => present,
+    comment => "Creed Bratton",
+    gid => "2005"
+    groups => ["accounting"],
+    shell => "/bin/bash",
+    home => "/home/cbratton",
+    managehome => true,
+}
+
+user { "dphilbin":
+    uid => "2006",
+    ensure => present,
+    comment => "Darryl Philbin",
+    gid => "2006"
+    groups => ["accounting"],
+    shell => "/bin/bash",
+    home => "/home/dphilbin",
+    managehome => true,
+}
+
+user { "abernard":
+    uid => "3001",
+    ensure => present,
+    comment => "Andy Bernard",
+    gid => "3001"
+    groups => ["sales"],
+    shell => "/bin/bash",
+    home => "/home/abernard",
+    managehome => true,
+}
+
+user { "plapin":
+    uid => "3002",
+    ensure => present,
+    comment => "Phyllis Lapin",
+    gid => "3002"
+    groups => ["sales"],
+    shell => "/bin/bash",
+    home => "/home/plapin",
+    managehome => true,
+}
+
+user { "shudson":
+    uid => "3003",
+    ensure => present,
+    comment => "Stanley Hudson",
+    gid => "3003"
+    groups => ["sales"],
+    shell => "/bin/bash",
+    home => "/home/shudson",
+    managehome => true,
+}
+
+user { "mpalmer":
+    uid => "3004",
+    ensure => present,
+    comment => "Meredith Palmer",
+    gid => "3004"
+    groups => ["sales"],
+    shell => "/bin/bash",
+    home => "/home/mpalmer",
+    managehome => true,
+}
+
+user { "kkapoor":
+    uid => "3005",
+    ensure => present,
+    comment => "Kelly Kapoor",
+    gid => "3005"
+    groups => ["sales"],
+    shell => "/bin/bash",
+    home => "/home/kkapoor",
+    managehome => true,
+}
+
+user { "pbeesly":
+    uid => "3006",
+    ensure => present,
+    comment => "Pam Beesly",
+    gid => "3006"
+    groups => ["sales"],
+    shell => "/bin/bash",
+    home => "/home/pbeesly",
+    managehome => true,
+}
+
+file { "/home/managers":
+    ensure => "directory",
+    owner => "mscott",
+    group => "managers",
+    mode => "0770",
+    recursive => "true",
+}
+
+file { "/home/accounting":
+    ensure => "directory",
+    owner => "amartin",
+    group => "accounting",
+    mode => "0770",
+    recursive => "true",
+}
+
+file { "/home/sales":
+    ensure => "directory",
+    owner => "abernard",
+    group => "sales",
+    mode => "0770",
+    recursive => "true",
+}
